ea9cab753973908aa94177aa1dbaaa59d97776bf
David Johns <djjohns42@gmail.com>
2025-11-05 23:11:38 -0600

    Adding env_vars.example.ps1

---
+++ env_vars.example.ps1
@@ -0,0 +1,23 @@
+# DO NOT WRITE IN THIS FILE!
+# THIS IS FOR MANUALLY SETTING ENVIRONMENTAL VARIABLES IN A LOCAL DEV ENV!
+# SET THESE AS SECRETS IN THE REPO'S SECRET STORE!
+# Example of how to set Environmental variables in windows using PowerShell.
+
+# Environmental Variables For Postgres Connection.
+$env:PG_HOST=""
+$env:PG_USER=""
+$env:PG_PASS=""
+$env:PG_SCHEMA=""
+
+
+# Environmental Variables For MS SQL Server Connection.
+$env:MSSQL_HOST=""
+$env:MSSQL_USER=""
+$env:MSSQL_PASS=""
+$env:MSSQL_SCHEMA=""
+
+# Environmental Variables For Teradata Connection.
+$env:TD_HOST=""
+$env:TD_USER=""
+$env:TD_PASS=""
+$env:TD_SCHEMA=""
